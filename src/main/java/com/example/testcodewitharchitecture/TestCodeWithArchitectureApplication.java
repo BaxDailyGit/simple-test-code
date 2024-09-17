9ce41bbfcf701d2988d35bcc1cf19a58d83ea88a
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-09-17 22:26:13 +0900

    init

---
+++ src/main/java/com/example/testcodewitharchitecture/TestCodeWithArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcodewitharchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCodeWithArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCodeWithArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testcodewitharchitecture/TestCodeWithArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcodewitharchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCodeWithArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
